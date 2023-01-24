de445ea46ecd527caecc34140d768aecd7354a70
Raja <official.rn88106@gmail.com>
2023-01-24 18:39:11 +0530

    Page Complete

--- src/App.js
+++ src/App.js
@@ -1,41 +1,27 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Component/Login";
+import Home from "./Component/Home";
+import Header from "./Component/Header"
+import Cart from "./Component/Cart";
+import "./Style/App.scss";
+
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+    <>
+     <Router>
+      <Header />
+     <Routes>
+     <Route path="/" element={<Login />} />
+     <Route path="/home" element={<Home />} />
+     <Route path="/cart" element={<Cart />} />
+</Routes>
+     </Router>
+     
+
+    
+
+    </>
   );
 }
 
